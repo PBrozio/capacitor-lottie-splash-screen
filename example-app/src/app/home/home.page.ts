7e8435ef1f3878ea73fc777511afede872fdc159
Phil Brozio <p.brozio@planzeit.de>
2025-11-03 08:55:12 +0100

    renamed example lottie; added new lottie file as asset; implemented button on home page to present lottie splash screen again button with other animation

--- example-app/src/app/home/home.page.ts
+++ example-app/src/app/home/home.page.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
+import { LottieSplashScreen } from '../../../../dist/esm';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton],
 })
 export class HomePage {
   constructor() {}
+
+  protected showSplashScreen(): void {
+    LottieSplashScreen.show({ animation: 'public/assets/lottie-runtime.json' });
+  }
 }
